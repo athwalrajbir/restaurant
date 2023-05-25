ddd9476d9d15cc9e07bf011b440cae76345349d1
Rajbir <rajbirathwal1@gmail.com>
2023-05-25 15:25:29 -0400

    Lorem added

--- src/component/basics/restaurant.js
+++ src/component/basics/restaurant.js
@@ -7,8 +7,13 @@ const Restaurant = () => {
         <div className="card">
           <div className="card-body">
             <span className="card-number card-circle subtle">1</span>
-            <span className="card-author subtle" style={{ color: "red" }}>
-              BreakFast
+            <span className="card-author subtle">BreakFast</span>
+            <h2>Maggie</h2>
+            <span className="card-description subtle">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
+              autem recusandae optio, dolorem amet quis harum veritatis, porro
+              perferendis quos excepturi sed suscipit aperiam error dolore!
+              Magni saepe libero perferendis.
             </span>
           </div>
         </div>
